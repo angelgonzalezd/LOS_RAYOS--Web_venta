836da2925f3da86acbb5fefefbd882b7dbc49c93
Angel Anderson Gonzalez Delgado <angel99vr@hotmail.com>
2021-02-24 18:06:27 -0500

    Version1.1.1

---
+++ web_venta/code..codigo/appweb/Pagina_web_Version_1.1.1/conexion.php
@@ -0,0 +1,11 @@
+<?php
+$conexion = mysqli_connect("127.0.0.1", "root", "", "bd_pgweb");
+
+//if (!$conexion) {
+  //  echo "Error: No se pudo conectar a MariaDB." . PHP_EOL;
+    //exit;
+//}
+
+//echo "Éxito: Se realizó una conexión apropiada a MariaDB! La base de datos datos_usuario es genial." . PHP_EOL;
+//echo "Información del host: " . mysqli_get_host_info($conexion) . PHP_EOL;
+
